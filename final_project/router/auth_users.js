83951c0d06bbd03369096ec936405af13f66bf98
yosief dagnachew <2122232425dd@gmail.com>
2025-10-01 15:06:09 +0300

    Task 9 -Delete a book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -52,6 +52,27 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     return res.status(404).json({message: "Book not found"});
   }
 });
+// Task 9: Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+const isbn = req.params.isbn;
+const username =req.session.authorization?.username;
+
+if (!username) {
+  return res.status(403).json({message: "Login required"});
+}
+
+if (books[isbn ]) {
+  if (books[isbn].reviews[username]) {
+    delete books[isbn].reviews[username];
+    return res.status(200).json({message: "Review deleted successfully"});
+  } else {
+    return res.status(404).json({message: "No review found for this user"});
+  }
+} else {
+  return res.status(404).json({message: "Book not found"});
+}
+
+});
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
